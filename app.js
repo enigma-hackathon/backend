cb76a7f8a859ffcbeae04f3ca3130c771f2cfb53
Nour Essoussi <nourcherifsoussi@gmail.com>
2019-04-27 13:33:32 -0400

    Api Users Ready

--- app.js
+++ app.js
@@ -6,17 +6,44 @@ var logger = require("morgan");
 var router = express.Router();
 
 var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
 var apiRouter = require("./routes/api");
 
 var app = express();
 
 // Firebase Set Up
 
-var firebase = require("firebase");
-var config = require("./config/keys");
+// var firebase = require("firebase");
+// var config = require("./config/keys");
+// const uuidv4 = require("uuid/v4");
 
-firebase.initializeApp(config);
+// firebase.initializeApp(config);
+
+// var database = firebase.database();
+
+// app.get("/users", (req, res) => {});
+// function writeUserData(userId, name, email, imageUrl) {
+//   firebase
+//     .database()
+//     .ref("users/" + userId)
+//     .set({
+//       username: name,
+//       email: email,
+//       profile_picture: imageUrl
+//     });
+// }
+
+// writeUserData(
+//   uuidv4(),
+//   "Nour Cherif Essoussi",
+//   "nourcherifsoussi@gmail.com",
+//   "https://scontent-mia3-1.xx.fbcdn.net/v/t1.0-9/49489234_2286860368227132_8734844250241368064_n.jpg?_nc_cat=106&_nc_ht=scontent-mia3-1.xx&oh=e3ec691887f9233600b94386d2d4d604&oe=5D2E29D2"
+// );
+// writeUserData(
+//   uuidv4(),
+//   "Mohamed Soussi",
+//   "mo@daycationapp.com",
+//   "https://scontent-mia3-1.xx.fbcdn.net/v/t1.0-9/15349672_10211488913042333_1732675283537726846_n.jpg?_nc_cat=100&_nc_ht=scontent-mia3-1.xx&oh=ac8c9846eb65d07fce995fcaa89c6c9c&oe=5D3D1827"
+// );
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -29,7 +56,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
 app.use("/api", apiRouter);
 
 // catch 404 and forward to error handler
--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,22 @@
 var express = require("express");
 var router = express.Router();
+
+var firebase = require("firebase");
 var config = require("../config/keys");
 
-/* GET Api page. */
+firebase.initializeApp(config);
 
-router.get("/", function(req, res, next) {
-  console.log("ASBOUUUUJA");
-  res.json(config);
+var database = firebase.database();
+
+router.get("/users", function(req, res, next) {
+  firebase
+    .database()
+    .ref("/users/")
+    .once("value")
+    .then(function(snapshot) {
+      // var username = (snapshot.val() && snapshot.val().username) || "Anonymous";
+      res.json(snapshot);
+    });
 });
 
 module.exports = router;
