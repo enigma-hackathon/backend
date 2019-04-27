97d8723a40d751747156d4720d2a694b384634c3
Nour Essoussi <nourcherifsoussi@gmail.com>
2019-04-27 14:29:06 -0400

    New User

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,9 @@
 var express = require("express");
 var router = express.Router();
+var bodyParser = require("body-parser");
+
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
 
 var firebase = require("firebase");
 var config = require("../config/keys");
@@ -17,6 +21,15 @@ router.get("/users", function(req, res, next) {
       // var username = (snapshot.val() && snapshot.val().username) || "Anonymous";
       res.json(snapshot);
     });
+});
+
+router.post("/users/new", function(req, res) {
+  var email = req.body.email;
+  var name = req.body.name;
+  var uid = req.body.uid;
+  var photo = req.body.photo;
+  console.log("User name = " + user_name + ", password is " + password);
+  res.json({ status: "Scuccess" });
 });
 
 module.exports = router;
